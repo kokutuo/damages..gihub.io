0e91e00ea25261646f4ab8913f5b2cd34889280b
yinjiaqi <edwardyin@yeahka.com>
2023-11-23 22:13:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,301 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-form
+      :model="formData"
+      label-width="100px"
+      class="demo-form-inline"
+    >
+      <el-form-item label="本金">
+        <el-input v-model="formData.total"></el-input>
+      </el-form-item>
+      <el-form-item label="利息起算日期">
+        <el-date-picker
+          type="date"
+          placeholder="选择日期"
+          style="width:100%;"
+          v-model="formData.startDate"
+          value-format="yyyy-MM-dd"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="利息终止日期">
+        <el-date-picker
+          type="date"
+          placeholder="选择日期"
+          style="width:100%;"
+          v-model="formData.endDate"
+          value-format="yyyy-MM-dd"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="lpr倍率">
+        <el-input v-model="formData.rate"></el-input>
+      </el-form-item>
+      <el-form-item label="lpr">
+        <el-select
+          v-model="formData.lprY"
+          placeholder="请选择活动区域"
+        >
+          <el-option
+            label="一年期"
+            value="oneY"
+          ></el-option>
+          <el-option
+            label="五年期"
+            value="fiveY"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="onSubmit"
+        >计算</el-button>
+        <el-button
+          type="primary"
+          @click="exportExcel"
+        >导出</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table
+      :summary-method="getSummaries"
+      :data="tableData"
+      show-summary
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="total"
+        label="本金"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="start"
+        label="利息起算日期"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="end"
+        label="利息截止日期"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="days"
+        label="计算天数"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="lpr"
+        label="LPR"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="interest"
+        label="利息"
+      >
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+import Excel from "./utils/excel";
+import Dayjs from "dayjs";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      visible: false,
+      formData: {
+        total: null,
+        startDate: "",
+        endDate: "",
+        rate: 1,
+        lprY: "oneY",
+        lprArr: [],
+      },
+      tableData: [],
+    };
+  },
+  mounted() {
+    this.lprArr = require("./assets/lpr.json");
+    var isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
+    Dayjs.extend(isSameOrBefore);
+    var isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
+    Dayjs.extend(isSameOrAfter);
+  },
+  methods: {
+    onSubmit() {
+      let res = [];
+      let validLpr = this.lprArr.filter((item) => {
+        return (
+          Dayjs(item.date).isSameOrBefore(Dayjs(this.formData.endDate)) &&
+          Dayjs(item.date).isSameOrAfter(
+            Dayjs(this.formData.startDate).subtract(30, "day")
+          )
+        );
+      });
+      for (let i = validLpr.length - 1; i >= 0; i--) {
+        const element = validLpr[i];
+
+        let days = 0, //间隔天数
+          start = "", // 起算日期
+          end = "", //截至日期
+          lpr = "", //lpr
+          interest = ""; //利息
+
+        if (i === validLpr.length - 1) {
+          days = this.diff(validLpr[i - 1].date, this.formData.startDate);
+          start = this.formData.startDate;
+          end = Dayjs(validLpr[i - 1].date)
+            .subtract(1, "day")
+            .format("YYYY-MM-DD");
+        } else if (i === 0) {
+          days = this.diff(this.formData.endDate, element.date) + 1;
+          start = element.date;
+          end = this.formData.endDate;
+        } else {
+          days = this.diff(validLpr[i - 1].date, element.date);
+          start = element.date;
+          end = Dayjs(validLpr[i - 1].date)
+            .subtract(1, "day")
+            .format("YYYY-MM-DD");
+        }
+
+        lpr = element[this.formData.lprY];
+
+        interest =
+          (Number(this.formData.total) *
+            Number(days) *
+            Number(lpr) *
+            Number(this.formData.rate)) /
+          365 /
+          100;
+
+        res.push({
+          total: this.formData.total,
+          start,
+          end,
+          days,
+          lpr,
+          interest,
+        });
+      }
+      console.log(
+        "🚀 ~ file: App.vue:86 ~ onSubmit ~ res:",
+        JSON.stringify(res)
+      );
+      this.tableData = this.mergeLPR(res);
+    },
+
+    exportExcel() {
+      let data = [...this.tableData];
+
+      // data.push({
+      //   total: "合计",
+      //   start: "",
+      //   end: "",
+      //   days: "",
+      //   lpr: "",
+      //   interest: this.round(
+      //     data.reduce((prev, curr) => {
+      //       const value = Number(curr.interest);
+      //       if (!isNaN(value)) {
+      //         return prev.interest + curr.interest;
+      //       } else {
+      //         return prev.interest;
+      //       }
+      //     }, 0)
+      //   ),
+      // });
+
+      console.log("🚀 ~ file: App.vue:195 ~ exportExcel ~ data:", data);
+
+      let header = [
+        "本金",
+        "利息起算日期",
+        "利息截止日期",
+        "计算天数",
+        "LPR",
+        "利息",
+      ];
+      Excel.exportExcel(
+        this.tableData,
+        "违约金计算表格" + Dayjs().format() + ".xlsx",
+        header
+      );
+    },
+
+    getSummaries(param) {
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = "合计";
+          return;
+        }
+        const values = data.map((item) => Number(item[column.property]));
+        if (!values.every((value) => isNaN(value))) {
+          sums[index] = this.round(
+            values.reduce((prev, curr) => {
+              const value = Number(curr);
+              if (!isNaN(value)) {
+                return prev + curr;
+              } else {
+                return prev;
+              }
+            }, 0)
+          );
+        } else {
+          sums[index] = "N/A";
+        }
+      });
+
+      return sums;
+    },
+
+    round(x) {
+      return (Math.round(x * (10 ^ 2)) / (10 ^ 2)).toFixed(2);
+    },
+
+    diff(date1, date2) {
+      return Dayjs(date1).diff(date2, "day");
+    },
+
+    mergeLPR(arr) {
+      if (arr.length === 0) return arr;
+
+      let result = [arr[0]];
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i].lpr === result[result.length - 1].lpr) {
+          result[result.length - 1].end = arr[i].end;
+          result[result.length - 1].days += arr[i].days;
+          result[result.length - 1].interest += arr[i].interest;
+        } else {
+          result.push(arr[i]);
+        }
+      }
+      return result;
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  background: #eeeeee9d;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 700px;
 }
 </style>
---
+++ src/utils/excel.js
@@ -0,0 +1,124 @@
+/* eslint-disable */
+
+// import XLSX from "xlsx";
+import * as XLSX from "xlsx";
+
+/**
+ * @description:
+ * @param {Object} json 服务端发过来的数据
+ * @param {String} name 导出Excel文件名字
+ * @return:
+ */
+function exportExcel(json, name, header) {
+  /* convert state to workbook */
+  var data = new Array();
+  var keyArray = new Array();
+
+  for (const key1 in json) {
+    if (json.hasOwnProperty(key1)) {
+      const element = json[key1];
+      var rowDataArray = new Array();
+      for (const key2 in element) {
+        if (element.hasOwnProperty(key2)) {
+          const element2 = element[key2];
+          rowDataArray.push(element2);
+          if (keyArray.length < getLength(element)) {
+            keyArray.push(key2);
+          }
+        }
+      }
+      data.push(rowDataArray);
+    }
+  }
+  if (header) {
+    keyArray = header;
+  }
+  data.splice(0, 0, keyArray);
+  console.log("🚀 ~ file: excel.js:34 ~ exportExcel ~ keyArray:", keyArray);
+  console.log("🚀 ~ file: excel.js:34 ~ exportExcel ~ data:", data);
+  const ws = XLSX.utils.aoa_to_sheet(data);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+  /* generate file and send to client */
+  XLSX.writeFile(wb, name + ".xlsx");
+}
+
+/**
+ * @description: 导入excel文件并返回数据
+ * @param {function} 回调函数参数data,dataRef,一个是数据，一个是exce表单的索引
+ * @return:
+ */
+function importExcel(callback) {
+  var inputObj = document.createElement("input");
+  inputObj.setAttribute("id", "file");
+  inputObj.setAttribute("type", "file");
+  inputObj.setAttribute("name", "file");
+  inputObj.setAttribute("style", "visibility:hidden");
+  inputObj.setAttribute("accept", ".xlsx,.xls,.csv");
+  inputObj.addEventListener("change", (evt) => {
+    const files = evt.target.files;
+    if (files && files[0])
+      _file(files[0], (data, dataRef) => {
+        callback(data, dataRef);
+      });
+  });
+  document.body.appendChild(inputObj);
+  inputObj.value;
+  inputObj.click();
+}
+
+/**
+ * @description: 处理文件
+ * @param {Object} file 文件对象
+ * @param {function} callback 回调函数
+ * @return:
+ */
+function _file(file, callback) {
+  const make_cols = (refstr) =>
+    Array(XLSX.utils.decode_range(refstr).e.c + 1)
+      .fill(0)
+      .map((x, i) => ({
+        name: XLSX.utils.encode_col(i),
+        key: i,
+      }));
+
+  /* Boilerplate to set up FileReader */
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    /* Parse data */
+    const bstr = e.target.result;
+    const wb = XLSX.read(bstr, {
+      type: "binary",
+    });
+    /* Get first worksheet */
+    const wsname = wb.SheetNames[0];
+    const ws = wb.Sheets[wsname];
+    /* Convert array of arrays */
+    const data = XLSX.utils.sheet_to_json(ws, {
+      header: 1,
+    });
+    /* Update state */
+    callback(data, make_cols(ws["!ref"]));
+  };
+  reader.readAsBinaryString(file);
+}
+
+/**
+ * @description: 获取map的长度
+ * @param {Object} obj map对象
+ * @return: map的长度
+ */
+function getLength(obj) {
+  var count = 0;
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+export default {
+  exportExcel,
+  importExcel,
+};
